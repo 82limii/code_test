cd3b11d059602ea66f0d4d4a3bd9b7dacb135735
82limii <82.limii@gmail.com>
2021-11-16 18:17:20 +0900

    2021.11.16

--- code_practice/src/code_practice/SelectTwo.java
+++ code_practice/src/code_practice/SelectTwo.java
@@ -5,13 +5,13 @@ import java.util.List;
 public class SelectTwo {
 	public int[] solution(int[] numbers) {
         int[] answer = {};
-        List<Integer> cal = [];
-        for(int i=0; i<numbers.length;i++) {
-        	for(int j=0; j<numbers.length;i++) {
-        		cal = numbers[i] + numbers[j];
-        		
-        	}
-        }
+//        List<Integer> cal = [];
+//        for(int i=0; i<numbers.length;i++) {
+//        	for(int j=0; j<numbers.length;i++) {
+//        		cal = numbers[i] + numbers[j];
+//        		
+//        	}
+//        }
         return answer;
     }
 	
