e0c6887fd5a5eef67151016547d134d276f54542
sanglim lee <82.limii@gmail.com>
2021-12-02 21:42:42 +0900

    Update NewId.java

--- code_practice/src/code_practice/NewId.java
+++ code_practice/src/code_practice/NewId.java
@@ -18,20 +18,35 @@ public class NewId {
         	dots += ".";
         }
 //        4단계 new_id에서 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
-        if(new_id.indexOf(".") == 0 || new_id.indexOf(".") == new_id.length()) {
-        	new_id = new_id.replace(".", "");
+        StringBuffer new_idBuffer = null;
+        if(new_id.indexOf(".") == 0 || new_id.lastIndexOf(".") == (new_id.length()-1)) {
+        	if(new_id.indexOf(".") == 0) {        		
+        		new_idBuffer = new StringBuffer(new_id);
+        		new_id = new_idBuffer.substring(1, (new_id.length()));
+        	} else if(new_id.lastIndexOf(".") == (new_id.length()-1)) {        		
+        		new_idBuffer = new StringBuffer(new_id);
+        		new_id = new_idBuffer.substring(0, (new_id.length()-1));
+        	}
         }
 //        5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
+        if(new_id.isEmpty()) {
+        	new_id += "a";
+        }
 //        6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
 //        	만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
+        if(new_id.length() >= 16) {
+        	new_idBuffer = new StringBuffer(new_id);
+        	new_id = new_idBuffer.substring(0, 15);
+        }
 //        7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
+        
         answer = new_id;
         return answer;
     }
 	
 	public static void main(String[] args) {
 		NewId method = new NewId();
-		String new_id = "..fea....ae...eagaea...";
+		String new_id = "..afeuahehalierhaetihithe;l..";
 		String answer = method.solution(new_id);
 		System.out.println(answer);
 	}
